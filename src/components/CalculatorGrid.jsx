17553cbf3e2e57e24b9bd4074cb1dd7824aaeb81
Andersonvb <andersonvalenciabermudez@gmail.com>
2022-11-20 10:43:34 -0500

    Add readme

--- src/components/CalculatorGrid.jsx
+++ src/components/CalculatorGrid.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../stylesheets/CalculatorGrid.css";
 import Button from "./Button.jsx";
 import { evaluate } from "mathjs";
